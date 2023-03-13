a365f5152af6849007f285f6e31212ab3bf7dd0e
bltszr <beltsazar.samosir@gmail.com>
2023-03-13 17:50:52 +0700

    Add options for other stuff

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import os
 import json
 import random
 import glob
+import sys
 
 import time
 from datetime import datetime
@@ -34,18 +35,26 @@ line_spacing_rules = {
   docx.enum.text.WD_LINE_SPACING.DOUBLE: 2,
 }
 
-DEFAULT = {
-  "spacing": Inches(0.5),
-  "spacing rule": 1,
-  "font": "Calibri",
-  "font size": Pt(12),
-  "page height": Mm(297),
-  "page width": Mm(210),
-  "left margin": Inches(1),
-  "right margin": Inches(1),
-  "top margin": Inches(1),
-  "bottom margin": Inches(1),
+line_spacing_map = {
+  "1.5": docx.enum.text.WD_LINE_SPACING.ONE_POINT_FIVE,
+  "single": docx.enum.text.WD_LINE_SPACING.SINGLE,
+  "double": docx.enum.text.WD_LINE_SPACING.DOUBLE,
 }
+
+DEFAULT = {
+  # "spacing": Inches(0.5),
+  # "spacing rule": 1,
+  # "font": "Calibri",
+  # "font size": Pt(12),
+  # "page height": Mm(297),
+  # "page width": Mm(210),
+  # "left margin": Inches(1),
+  # "right margin": Inches(1),
+  # "top margin": Inches(1),
+  # "bottom margin": Inches(1),
+}
+
+backrounds = []
 
 to_project_dir = lambda path : os.path.join('outputs', f"{os.path.basename(path)}.{int(time.mktime(datetime.now().timetuple()))}.d")
 
@@ -57,9 +66,12 @@ def save(img, bboxes, dest_folder, page_number):
     json.dump(bboxes, f)
   print(f'Saved \'{img_filename}\'')
 
-def create_page(page_width, page_height, bg=None):
-  if bg != None:
-    img = Image.open(random.choice(glob.glob(os.path.join(bg, '*'))))\
+
+def create_page(page_width, page_height, bg=False):
+  global backgrounds
+  if bg:
+    background = random.choice(backgrounds)
+    img = Image.open(background)\
                .resize((page_width, page_height))\
                .convert("RGBA")
   else:
@@ -77,7 +89,6 @@ def get_wrapped_text(text: str,
                      line_length: int):
   lines = ['']
   for word in text.split():
-    # print(lines)
     line = f'{lines[-1]} {word}'.strip()
     if font.getlength(line, direction="rtl") <= line_length:
       lines[-1] = line
@@ -109,7 +120,7 @@ def get_font(run, font_dict):
   try:
     run_font_size = get_font_size(run.style)
   except (TypeError, AttributeError):
-    run_font_size = DEFAULT["font size"]
+    run_font_size = DEFAULT["font-size"]
 
   try:
     run_font_name = get_font_name(to_px(run.style))
@@ -125,13 +136,13 @@ def get_spacing(paragraph):
       try:
         spacing = from_px(get_font_size(paragraph.style) * paragraph.paragraph_format.line_spacing)
       except (TypeError, AttributeError, IndexError) as e:
-        spacing = Emu(DEFAULT["spacing"] * DEFAULT["spacing rule"])
+        spacing = Emu(DEFAULT["spacing"] * DEFAULT["spacing-rule"])
     else:
       spacing = paragraph.paragraph_format.line_spacing * line_spacing_rules[
         paragraph.paragraph_format.line_spacing_rules
       ]
   except (TypeError, AttributeError) as e:
-    spacing = Emu(DEFAULT["spacing"] * DEFAULT["spacing rule"])
+    spacing = Emu(DEFAULT["spacing"] * DEFAULT["spacing-rule"])
   return spacing
 
 def get_indents(paragraph):
@@ -147,27 +158,23 @@ def get_indents(paragraph):
 def process_txt(args, font):
   dest_folder = to_project_dir(args.path)
   os.makedirs(dest_folder, exist_ok=True)
-  
   page_width, page_height,\
     left_margin, right_margin,\
     top_margin, bottom_margin = map(to_px,
-                                    (DEFAULT['page width'],
-                                     DEFAULT['page height'],
-                                     DEFAULT['left margin'],
-                                     DEFAULT['right margin'],
-                                     DEFAULT['top margin'],
-                                     DEFAULT['bottom margin']))
-  
+                                    (DEFAULT['page-width'],
+                                     DEFAULT['page-height'],
+                                     DEFAULT['left-margin'],
+                                     DEFAULT['right-margin'],
+                                     DEFAULT['top-margin'],
+                                     DEFAULT['bottom-margin']))
   page_number = 1
-  # print((page_width, page_height))
   # Iterate over each paragraph in the document
   img, draw, bboxes = create_page(page_width, page_height, args.background)
+  
   cum_spacing = top_margin
   # Extract the text and style of the paragraph
-  
-  # print(paragraph.paragraph_format.line_spacing)
-  # print(paragraph.paragraph_format.line_spacing_rule)
-  par_spacing = to_px(Emu(DEFAULT['spacing'] * DEFAULT['spacing rule']))
+  par_spacing = to_px(Emu(DEFAULT['spacing'] \
+                          * line_spacing_rules[DEFAULT['spacing-rule']]))
   right_indent = 0
   left_indent = 0
   for j, run in enumerate(open(args.path, 'r')):
@@ -234,7 +241,6 @@ def process_doc(args, font_dict):
   
   page_number = 1
   text_page_height = page_height - top_margin - bottom_margin
-  # print((page_width, page_height))
   # Iterate over each paragraph in the document
   img, draw, bboxes = create_page(page_width, page_height, bg)
   cum_spacing = top_margin
@@ -244,21 +250,16 @@ def process_doc(args, font_dict):
     # Extract the text and style of the paragraph
     right_indent, left_indent = get_indents(paragraph)
     style = paragraph.style.name
-    
-    # print(paragraph.paragraph_format.line_spacing)
-    # print(paragraph.paragraph_format.line_spacing_rule)
     par_spacing = to_px(get_spacing(paragraph))
     for j, run in enumerate(paragraph.runs):
+      
       if run._element.xpath('w:lastRenderedPageBreak'):
         save(img, bboxes, dest_folder, page_number)
         img, draw, bboxes = create_page(page_width, page_height)
-        # draw.rectangle([0, 0, 500, 500], outline="Red")
         cum_spacing = top_margin
         page_number += 1
       if page_number > args.end_page:
         break
-      # print(page_number)
-      # print(run.text)
       font = get_font(run, font_dict)
       text = get_wrapped_text(run.text,
                               font,
@@ -284,7 +285,6 @@ def process_doc(args, font_dict):
         y = cum_spacing
         if args.warn and y > page_height:
           print(f"[WARN] Ran off page at line {line}\n")
-        # line = get_display(reshaper.reshape(line))
         draw.text((x, y), line,
                   font=font,
                   fill=(0, 0, 0),
@@ -297,56 +297,79 @@ def process_doc(args, font_dict):
         cum_spacing += par_spacing
 
 def main(args):
-  DEFAULT.update({"font": args.default_font,
-                  "font size": Pt(args.default_font_size),
-                  "spacing": Inches(args.default_spacing)})
+  global backgrounds
+  backgrounds = glob.glob(os.path.join(args.background, '*'))
   if args.alpha:
     args.min_alpha = args.alpha
     args.max_alpha = args.alpha
+  if args.margin:
+    args.bottom_margin = args.margin
+    args.top_margin = args.margin
+    args.right_margin = args.margin
+    args.left_margin = args.margin
+
+  DEFAULT.update({"font": args.font,
+                  "font-size": Pt(args.font_size),
+                  "spacing": Inches(args.spacing),
+                  "left-margin": Inches(args.left_margin),
+                  "right-margin": Inches(args.right_margin),
+                  "top-margin": Inches(args.top_margin),
+                  "bottom-margin": Inches(args.bottom_margin),
+                  "page-width": Mm(args.page_width),
+                  "page-height": Mm(args.page_height),
+                  "spacing-rule": line_spacing_map[args.spacing_rule]})
   configuration = {
     'delete_harakat': False,
     'support_ligatures': True,
     'RIAL SIGN': True,  # Replace ر ي ا ل with ﷼
   }
+  
   with open('fonts') as fp:
     font_dict = json.load(fp)
-  WARN = args.warn
+    
   reshaper = ArabicReshaper(configuration=configuration)
   if args.path.endswith(".docx"):
     process_doc(args, font_dict)
   elif args.path.endswith(".txt"):
-    if os.path.exists(args.default_font):
-      fontpath = args.default_font
+    if os.path.exists(args.font):
+      fontpath = args.font
     else:
-      fontpath = font_dict[args.default_font]
+      fontpath = font_dict[args.font]
     process_txt(args, ImageFont.truetype(fontpath,
-                                         size=to_px(DEFAULT['font size'])))
+                                         size=to_px(DEFAULT['font-size'])))
           
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Synthesizer for OCR data")
-  parser.add_argument("path", type=str,
-                      help="Path to pdf or txt file")
-  parser.add_argument("--start-page", type=int,
-                      help="Starter page to parse",
-                      default=0)
+  parser.add_argument("path", type=str, help="Path to pdf or txt file")
+  parser.add_argument("--start-page", type=int, help="Starter page to parse",default=0)
   parser.add_argument("--end-page", type=int,
-                      help="Starter page to parse, will be ignored if file is txt",
-                      default=math.inf)
-  parser.add_argument("--default-font", type=str,
+                      help="Starter page to parse, will be ignored if file is txt", default=math.inf)
+  parser.add_argument("--font", type=str,
                       help="Override default font",
-                      default=DEFAULT['font'])
-  parser.add_argument("--default-font-size", type=int,
-                      help="Override default font size in pt",
-                      default=DEFAULT['font size'].pt)
-  parser.add_argument("--default-spacing", type=float,
-                      help="Override default spacing rules",
-                      default=DEFAULT['spacing'].inches)
+                      default='Calibri')
+  parser.add_argument("--font-size", type=int,
+                      help="Override default font size (pt)", default=12)
+  parser.add_argument("--spacing", type=float,
+                      help="Override default spacing size (inches)", default=0.5)
+  parser.add_argument("--spacing-rule", choices=['1.5', 'double', 'single'], default='single')
+
+  
   parser.add_argument("--min-alpha", type=float, default=0.85,
                       help="Minimum alpha to choose for text")
   parser.add_argument("--max-alpha", type=float, default=1.0,
                       help="Maximum alpha to choose for text")
   parser.add_argument("--alpha", type=float, default=None,
                       help="Single alpha value for all text. Overrides min-alpha and max-alpha")
+
+  parser.add_argument("--left-margin", type=float, default=1, help="Override left margin (inches)")
+  parser.add_argument("--right-margin", type=float, default=1, help="Override right margin (inches)")
+  parser.add_argument("--top-margin", type=float, default=1, help="Override top margin (inches)")
+  parser.add_argument("--bottom-margin", type=float, default=1, help="Override bottom margin (inches)")
+  parser.add_argument("--margin", type=float, default=None, help="Override all margins (inches)")
+  
+  parser.add_argument("--page-width", type=float, default=210, help="Override page width (mm)",)
+  parser.add_argument("--page-height", type=float, default=297, help="Override page height (mm)")
+  
   parser.add_argument("--warn", action="store_true", help="Emit warnings")
   parser.add_argument("--background", type=str, help="Path to folder of backgrounds")
   
