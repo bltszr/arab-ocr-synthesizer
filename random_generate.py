309648a893e21c2c2f20435cf8baf8b9c51677ba
bltszr <beltsazar.samosir@gmail.com>
2023-05-03 17:00:03 +0700

    Add more options

--- random_generate.py
+++ random_generate.py
@@ -10,7 +10,8 @@ import docx
 
 font_sizes = range(17, 30)
 
-def generate(path, iters, bg_path, fonts):
+def generate(path, iters, bg_path, fonts,
+             min_spacing, max_spacing):
   if path.endswith('.txt'):
     docs = [path]
   elif os.path.isdir(path):
@@ -20,7 +21,7 @@ def generate(path, iters, bg_path, fonts):
   
   for iter in range(iters):
     font_size = random.choice(font_sizes)
-    spacing = random.uniform(0.8, 1.5) * docx.shared.Pt(font_size).inches
+    spacing = random.uniform(min_spacing, max_spacing) * docx.shared.Pt(font_size).inches
     try:
       doc = random.choice(docs)
     except IndexError as e:
@@ -48,7 +49,14 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="Random generator")
   parser.add_argument('path', type=str, help='Path to docs')
   parser.add_argument('iters', type=int, help='Number of docs to generate')
-  parser.add_argument('--bg-path', type=str, help='Path to bg', default='Backgrounds')
-  parser.add_argument('--fonts', type=str, help='Path to fonts', default='fonts.d')
+  parser.add_argument('--bg-path', type=str, help='Path to bg',
+                      default='Backgrounds')
+  parser.add_argument('--fonts', type=str, help='Path to fonts',
+                      default='fonts.d')
+  parser.add_argument('--min-spacing', type=int,
+                      help='Minimum space', default=0.8)
+  parser.add_argument('--max-spacing', type=float,
+                      help='Maximum space', default=1.5)
   args = parser.parse_args()
-  generate(args.path, args.iters, args.bg_path, args.fonts)
+  generate(args.path, args.iters, args.bg_path, args.fonts,
+           args.min_spacing, args.max_spacing)
